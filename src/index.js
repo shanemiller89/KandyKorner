4cc9c86dce534eafccbab49054af262e9a0be789
Shane Miller <shanemiller89@gmail.com>
2019-07-12 14:34:43 -0500

    Added Nav Bar

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React, {Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import KandyKorner from './components/KandyKorner.js'
+import { BrowserRouter as Router } from "react-router-dom"
 
-ReactDOM.render(<KandyKorner />, document.getElementById('root'));
+ReactDOM.render(<Router><KandyKorner /></Router>, document.getElementById('root'));
